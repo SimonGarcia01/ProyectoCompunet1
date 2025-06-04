a9a66bf7795a8e04a83041a5bef48dfb57108766
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2025-06-04 10:04:16 -0500

    Restarting build

--- NumerosPerfectos/workers/src/main/java/SuscriberI.java
+++
@@ -1,9 +0,0 @@
-import com.zeroc.Ice.Current; 
-
-public class SuscriberI implements Demo.Suscriber {
-    
-    @Override
-    public void onUpdate(String msg, Current current){
-	System.out.println(msg);
-    }
-}
